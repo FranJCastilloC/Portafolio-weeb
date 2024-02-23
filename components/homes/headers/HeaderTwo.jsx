c5d988c0946fcdf59f3d761daa14d2543015b548
Francisco Castillo <149974617+FranJCastilloC@users.noreply.github.com>
2024-02-23 19:46:43 -0400

    12

--- components/homes/headers/HeaderTwo.jsx
+++ components/homes/headers/HeaderTwo.jsx
@@ -60,8 +60,8 @@ export default function HeaderTwo() {
               <div className="bostami-header-logo">
                 <Link className="site-logo" href="/">
                   <Image
-                    width={745}
-                    height={772}
+                    width={153}
+                    height={32}
                     src="/assets/img/logo/Palmera-2.png"
                     alt="logo"
                   />
