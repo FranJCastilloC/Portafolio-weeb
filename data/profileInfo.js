56b7e229b9f97c0b74ec9fb1b0688ac0d8a752d5
Francisco Castillo <149974617+FranJCastilloC@users.noreply.github.com>
2024-09-24 08:51:31 -0400

    Updated

--- data/profileInfo.js
+++ data/profileInfo.js
@@ -3,7 +3,7 @@
 
 export const profileInfo = {
    name:'Francisco Castillo',
-   title:'SAP Master Data | Data Analyst | Industrial Engineer | Palmera Analytics' ,
+   title:' Data Science | Risk Analyst | Master Data | Data Analyst | Industrial Engineer | Palmera Analytics' ,
    imageSrcOne:'/assets/img/parsonal-info/parson-img-1.png',
    imageSrcTwo:'/assets/img/parsonal-info/parson-img-3.png',
    imageSrcThree:'/assets/img/parsonal-info/parson-img-2.png',
