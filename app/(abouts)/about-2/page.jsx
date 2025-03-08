7d60df75e270716d34765a7a76595bad18be64e7
Francisco Castillo <149974617+FranJCastilloC@users.noreply.github.com>
2025-03-08 13:13:15 -0400

    approved

--- app/(abouts)/about-2/page.jsx
+++ app/(abouts)/about-2/page.jsx
@@ -2,14 +2,12 @@ import AboutTwo from "@/components/homes/about/AboutTwo";
 import HeaderTwo from "@/components/homes/headers/HeaderTwo";
 import PersonalInfoTwo from "@/components/homes/personalInfo/PersonalInfoTwo";
 import React from "react";
-
 export const metadata = {
-  title: "About || Bostami - Professional Portfolio NextJS Template",
+  title: "About || Bostami - Professional portfolio NextJS Template",
   description:
-    "Discover Bostami, the most impressive portfolio template for work showcase, blog",
+    "Discover Botami,the most impressive portfolio template for work showcase, blog",
 };
-
-export default function Page() {
+export default function page() {
   return (
     <>
       <div className="page-wrapper page-wrapper-2">
@@ -17,7 +15,6 @@ export default function Page() {
         <div className="bostami-page-area z-index-3">
           <div className="container">
             <AboutTwo />
-            <PersonalInfoTwo />
           </div>
         </div>
       </div>
